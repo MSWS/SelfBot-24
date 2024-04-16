1544852ad9179f91dad6dc80c10326f88a7682d0
MSWS <imodmaker@gmail.com>
2024-04-16 01:16:13 -0700

    Init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+# 1. Choose a lightweight Node.js base image 
+FROM node:lts-alpine
+
+# 2. Create a working directory inside the container
+WORKDIR /app
+
+# 3. Copy your application code
+COPY package.json package-lock.json ./
+COPY src ./
+
+# 4. Install dependencies
+RUN npm install
+
+# 6. Specify the command to start your application
+CMD [ "node", "src/bot.js" ] 
---
+++ src/bot.js
@@ -0,0 +1,55 @@
+import 'dotenv/config'
+import OpenAI from 'openai';
+
+import { Client, Message } from 'discord.js-selfbot-v13';
+const client = new Client();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_TOKEN
+})
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+client.on('ready', async () => {
+    console.log(`${client.user.username} is ready!`);
+})
+
+client.on("messageCreate", async message => {
+    if (message.guildId === "161245089774043136") {
+        processEgoMessage(message);
+    }
+
+    if (message.author.id != client.user.id)
+        return;
+    if (!message.content.startsWith(":ed "))
+        return;
+    const msg = message.content.substring(": ed".length)
+    message.delete();
+    message.channel.sendTyping();
+
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [
+            {
+                role: 'user',
+                content: 'Rewrite the following into a sentence in the style of a person who feels deeply envious but tries to mask it with false enthusiasm. Use overly flowery language, a hint of sarcasm, and a focus on their own perceived misfortune.'
+            },
+            {
+                role: 'user',
+                content: msg
+            }],
+        model: 'gpt-3.5-turbo'
+    });
+    const response = chatCompletion.choices[0].message;
+    console.log(response);
+    message.channel.send(response);
+});
+
+const re = /\bms\b/;
+
+function processEgoMessage(message) {
+    if (!re.test(message.content.toLowerCase()))
+        return;
+    message.react('👀')
+}
+
+client.login(DISCORD_TOKEN);
